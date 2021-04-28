c35f604dbc9a916b272a8957d50c4a3a760d9072
Jamtechdev <jamtechba@gmail.com>
2021-04-28 13:55:46 +0530

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {MatIconModule} from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {RouterModule} from '@angular/router';
+const routes = [
+  {
+      path: 'app',
+      component: AppComponent
+  },
+];
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    RouterModule.forChild(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
